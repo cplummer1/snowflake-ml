894d45224f5472d8d8a908c038fb596bcdd9b6ba
cplummer@harkinsbuilders.com <cplummer@harkinsbuilders.com>
2025-05-09 09:04:26 -0400

    Push so Ben can look at file

--- snowflake-test.py
+++
@@ -1,6 +0,0 @@
-from safety_inspections_ml.utils.snowflake.SnowflakeConnection import SnowflakeConnection
-sf = SnowflakeConnection()
-
-query = sf.load_sql_query('utils/snowflake/queries/test.sql')
-
-print(sf.execute_query(query))
